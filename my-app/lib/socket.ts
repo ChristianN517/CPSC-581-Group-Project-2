bcd18de6c0d8096f0b85801a6f114d8a4838e8ab
Grace <graceeilori@gmail.com>
2026-03-09 17:29:47 -0600

    server linked

--- my-app/lib/socket.ts
+++ my-app/lib/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
+// http://localhost:3001
+export const socket = io("https://bricks-9y9l.onrender.com", {
     autoConnect: false,
 });
